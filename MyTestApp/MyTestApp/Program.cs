fabef2bf14afeafd3ebc032a274b00fe1c313437
Pete Riley <petetheriley@gmail.com>
2017-05-31 14:07:36 +0100

    Version 1.0.0

---
+++ MyTestApp/MyTestApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Version 1.0.0 ");
+            Console.WriteLine("press any key");
+            Console.ReadKey();
+        }
+    }
+}
